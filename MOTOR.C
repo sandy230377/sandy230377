afefbaccd5e8be395ece4c94ae6a6c8267b91d8e
sandy230377 <93254569+sandy230377@users.noreply.github.com>
2021-10-27 15:07:50 +0530

    Add files via upload

---
+++ MOTOR.C
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ void main()
+ {  int i=10;
+ printf("enter the value");
+ scanf("2d",&i);
+ if(i<0)
+ {
+ printf("motor on");
+ {if(i!=0)
+ printf("motor off");
+ getch();
+ }
+
+ }}
